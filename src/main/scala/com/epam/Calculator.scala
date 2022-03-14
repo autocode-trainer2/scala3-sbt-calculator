3b83f97e5171df76d1bb86217ce0291923481a50
Kirill Peshin <kirill_peshin@epam.com>
2022-03-14 12:40:31 +0300

    coverage

--- src/main/scala/com/epam/Calculator.scala
+++ src/main/scala/com/epam/Calculator.scala
@@ -1,11 +1,11 @@
 package com.epam
 
 object Calculator {
-  def add(x: Int, y: Int): Int = x + y
+  def add(x: Int, y: Int): Int = x + y + 1
 
   def subtract(x: Int, y: Int): Int = x - y
 
-  def multiply(x: Int, y: Int): Int = x * y
+  def multiply(x: Int, y: Int): Int = x * y + 1
 
   def divide(x: Int, y: Int): Int = {
     if (y == 0) {
